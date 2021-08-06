833fd6641fe3ed1f34a997a6f3985d0464e5c79b
dev.rehmanali <82761490+dev-rehmanali@users.noreply.github.com>
2021-08-06 10:17:46 +0500

    Added Accounts Source File
    code of accounts source file

---
+++ Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3._2
+{
+    class Account
+    {
+        private decimal balance;
+        private string name;
+        
+        public string Name 
+        {
+
+            get 
+            {
+                return name;
+            }
+        }
+
+        public Account(string name, decimal balance) {
+
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public bool Deposit(decimal amount) {
+
+            if (amount <= 0)
+            {
+
+                return false;
+            }
+            else { 
+            
+                this.balance += amount;
+                return true;
+            }
+
+        }//Deposit
+
+        public bool Withdraw(decimal amount) {
+
+            if (amount < 0 || amount > this.balance)
+            {
+
+                return false;
+            }
+            else {
+
+                this.balance -= amount;
+                return true;
+            }
+
+        }//Withdraw
+
+        public void Print() {
+
+            Console.WriteLine("Account Name is: " + this.name);
+            Console.WriteLine("Account Balance is: " + this.balance);
+        }
+
+
+    }
+}
